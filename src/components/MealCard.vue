661138f763a4584c52fcb118b57def8b49dc8973
Mohamed Ammar <mohamed.ammar@asuresoftware.com>
2024-05-09 23:37:52 -0500

    refactor: Add MealCard componenet

---
+++ src/components/MealCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-white shadow rounded-xl">
+    <img :src="image" :alt="title" class="rounded-t-xl w-full h-64 object-cover" />
+    <div class="p-2">
+      <h3 class="font-semibold py-1">{{ title }}</h3>
+      <div class="flex item-center justify-between py-2">
+        <a
+          :href="youtube"
+          target="_blank"
+          class="p-3 rounded border-2 border-red-700 hover:bg-red-600 hover:text-white transition-colors"
+          >Youtube</a
+        >
+        <router-link
+          :to="{ name: 'mealDetails', params: { id: id} }"
+          class="p-3 rounded border-2 border-green-700 hover:bg-green-600 hover:text-white transition-colors"
+        >
+          Details
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    image: String,
+    title: String,
+    youtube: String,
+    id: String,
+})
+
+</script>
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,39 +1,24 @@
 <template>
   <div class="flex flex-col p-8 h-100">
-    <div>
+    <div class="flex justify-center p-6">
       <input
         type="text"
-        class="rounded border-2 border-grey-200 w-full"
+        class="rounded border-2 border-grey-200 w-5/6"
         placeholder="Find a Meal"
         v-model="inputString"
         @change="getMealsByName"
       />
     </div>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-8">
-      <div v-for="meal in appStore.mealsByName" :key="meal.id" class="bg-white shadow rounded-xl">
-        <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl w-full h-80 object-cover" />
-        <div class="p-2">
-          <h3 class="font-semibold py-1">{{ meal.strMeal }}</h3>
-          <p class="py-2">
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
-          </p>
-          <div class="flex item-center justify-between py-2">
-            <a
-              :href="meal.strYoutube"
-              target="_blank"
-              class="p-3 rounded border-2 border-red-700 hover:bg-red-600 hover:text-white transition-colors"
-              >Youtube Link</a
-            >
-            <router-link
-              :to="{ name: 'mealDetails', params: { id: meal.idMeal } }"
-              class="p-3 rounded border-2 border-green-700 hover:bg-green-600 hover:text-white transition-colors"
-            >
-              Details
-            </router-link>
-          </div>
-        </div>
-      </div>
+    <div class="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 gap-8">
+      <MealCard
+        v-for="meal in appStore.mealsByName"
+        :key="meal.idMeal"
+        :title="meal.strMeal"
+        :image="meal.strMealThumb"
+        :youtube="meal.strYoutube"
+        :id="meal.idMeal"
+      />
     </div>
   </div>
 </template>
@@ -44,6 +29,7 @@ import { useRoute, useRouter } from 'vue-router';
 import { fetchMealsByName } from '../dataClient.js';
 import { useAppStore } from '../store/store.js';
 import MealDetails from './MealDetails.vue';
+import MealCard from '../components/MealCard.vue';
 
 const inputString = ref('');
 const appStore = useAppStore();
@@ -64,4 +50,13 @@ onMounted(() => {
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+input {
+  &:focus {
+    // border-color: green;
+    // outline: orange;
+    --tw-ring-color: green;
+    border-color: green;
+  }
+}
+</style>
